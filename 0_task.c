0a430a215c9c3cfc748d3f4dbf563d9f89ad6aeb
toleman84 <gdponcemarsiglia@gmail.com>
2022-12-18 14:30:51 -0800

    arreglos

--- 0_task.c
+++ 0_task.c
@@ -1,48 +1,5 @@
 #include "monty.h"
 
-<<<<<<< HEAD
-#define MAX_STACK_SIZE 100
-
-int stack[MAX_STACK_SIZE];
-int stack_top = -1;
-
-void push(int value) {
-if (stack_top < MAX_STACK_SIZE - 1) {
-stack_top++;
-stack[stack_top] = value;
-}
-else {
-printf("Error: Stack is full\n");
-}
-}
-
-void pall() {
-for (int i = stack_top; i >= 0; i--) {
-printf("%d\n", stack[i]);
-}
-}
-
-int main(int argc, char* argv[]) {
-char* opcode = argv[1];
-int value = 0;
-
-if (argc == 3) {
-value = atoi(argv[2]);
-}
-
-if (strcmp(opcode, "push") == 0) {
-push(value);
-}
-else if (strcmp(opcode, "pall") == 0) {
-pall();
-}
-else {
-printf("Error: Invalid opcode\n");
-return EXIT_FAILURE;
-}
-
-return 0;
-=======
 /**
  * push - pushes an element to the stack
  * @stack: the stack
@@ -92,6 +49,5 @@ void pall(stack_t **stack, unsigned int line_number)
 		printf("%d\n", current->n);
 		current = current->next;
 	}
->>>>>>> c0f94fea3728778b743d0a236d61bf20f5e7a1fd
 }
 
