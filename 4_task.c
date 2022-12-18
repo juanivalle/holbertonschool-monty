52dad99bacd07d27cb1e9ef5fc703a1cd5e8a14b
toleman84 <gdponcemarsiglia@gmail.com>
2022-12-17 18:34:23 -0800

    share with partner

---
+++ 4_task.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - add elements on the stack
+ * @stack: 
+ * @line_number: 
+ *
+ */
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L %u: can't aad, stack too short\n",
+			line_number);
+		free(stack);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->next->n += (*stack)->next->n;
+	pop(stack, line_number)i;
+}
+
