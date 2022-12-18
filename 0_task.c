c0f94fea3728778b743d0a236d61bf20f5e7a1fd
toleman84 <gdponcemarsiglia@gmail.com>
2022-12-17 19:13:09 -0800

    share with partner

--- 0_task.c
+++ 0_task.c
@@ -12,7 +12,7 @@ void push(stack_t **stack, unsigned int line_number)
 
 	if (!stack)
 	{
-		fprintf(stderr, "L%u: invalid stack\n", line_number);
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	new = malloc(sizeof(*new));
